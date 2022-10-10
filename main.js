1ea5604e122727efb4cb5b70aa6dfb7aa15bf147
Marsenok <mariia.yumasheva@yandex.ru>
2022-10-10 10:44:40 +0300

    First JS commit

---
+++ main.js
@@ -0,0 +1,37 @@
+let mas = 'message';
+let chik;
+chik = mas;//присваиваем именно чику мас а не наоброт
+console.log(chik);
+let $ = 10;
+let _ = 15;
+console.log(_ - $);
+const myBd = '06.04.2001'; //нельзя изменять, будет ошибка
+console.log(myBd);
+const COLOR_RED = "#f00"; //капс для устоявщихся обозначений,псевдоним для жестко закодированный обозначений
+let color = COLOR_RED;
+console.log(color);
+let NowTime = '11.34'; // разный регистр для изменяемой переменной
+console.log(NowTime);
+let Name = 'Maria ';
+let SecondName = 'Yumasheva ';
+let Age = 21;
+console.log(Name + SecondName + Age); //правильное название переменных
+
+let name = '1';
+console.log(`Hi ,${name+1}`);//обратные кавычки позволяют вставить {}
+let True = 4 > 1; //булевы
+let False = 5 < 1;
+console.log(True);
+console.log(False);
+let SeeWhatHappend = NaN ** 0;
+console.log(SeeWhatHappend);//=1
+console.log('SeeWhatHappend');//= seewhathappend
+console.log(1 + 3 + '4');//44
+console.log(10 / '1' - '4');//6
+console.log(true);//1
+let num = '22';
+console.log(+num + 1);//23
+a=num++;
+console.log(a);//22
+b = num;
+console.log(b);//23
